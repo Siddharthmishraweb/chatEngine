14d589b467fd66dda76f491c244dcad70d69a22e
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2024-02-23 23:43:48 +0530

    Added changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
 
 const MongoStore = require('connect-mongo')(session);
-const scssMiddleware = require('sass-middleware');
+// const scssMiddleware = require('sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
@@ -23,13 +23,13 @@ chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
 
 
-app.use(scssMiddleware({
-   src: './assets/scss',
-   dest: './assets/css',
-   debug: true,
-   outputStyle: 'extended',
-   prefix:'/css'
-}))
+// app.use(scssMiddleware({
+//    src: './assets/scss',
+//    dest: './assets/css',
+//    debug: true,
+//    outputStyle: 'extended',
+//    prefix:'/css'
+// }))
 app.use(express.urlencoded());
 
 app.use(cookieParser());
