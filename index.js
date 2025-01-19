5f7164c4ccb1bf8e3629de8b62251be5a334de2d
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2025-01-19 10:35:13 +0530

    Added redis

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const redisdb = require('./config/redis');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
